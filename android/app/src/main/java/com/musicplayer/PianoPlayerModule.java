63e987093f9a736271ebdee9ec3025cf73f4dee4
attila-sandor-inceptech <attila.sandor@incepteam.com>
2021-07-28 22:15:10 +0200

    add ios module

--- android/app/src/main/java/com/musicplayer/PianoPlayerModule.java
+++ android/app/src/main/java/com/musicplayer/PianoPlayerModule.java
@@ -1,192 +1,110 @@
 package com.musicplayer;
 
 import android.media.AudioAttributes;
-import android.media.AudioManager;
 import android.media.SoundPool;
-import android.media.SoundPool.OnLoadCompleteListener;
-import android.os.Build;
-import android.util.Log;
-import android.widget.ScrollView;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public class PianoPlayerModule extends ReactContextBaseJavaModule {
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private final HashMap<String, Integer> soundIds = new HashMap<>();
+    private SoundPool soundPool;
+    private HashMap<Integer, Integer> streamIds = new HashMap<>();
 
-  private final ReactApplicationContext reactContext;
-
-  private SoundPool soundPool;
-  private ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-  private HashMap<String, Integer> soundIds = new HashMap<String, Integer>();
-  private HashMap<Integer, Integer> streamIds = new HashMap<Integer, Integer>();
-  private ScheduledFuture[] scheduledFutures;
-
-  private int totalLoadedSounds = 0;
-  private int totalLoadableSounds = 0;
-
-  @Override
-  public String getName() {
-    return "PianoPlayerModule";
-  }
-
-  public PianoPlayerModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-    initializeSoundPool();
-    loadSounds();
-  }
-
-  private void initializeSoundPool() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      soundPool =
-        new SoundPool.Builder()
-          .setMaxStreams(6)
-          .setAudioAttributes(
-            new AudioAttributes.Builder()
-              .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-              .build()
-          )
-          .build();
-    } else soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
-  }
-
-  private void sendEvent(
-    ReactApplicationContext reactContext,
-    String eventName,
-    int i
-  ) {
-    WritableMap params = Arguments.createMap();
-    params.putInt("num", i);
-    reactContext
-      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-      .emit(eventName, params);
-  }
-
-  private void loadSounds() {
-    for (int i = 0; i <= 14; i++) {
-      String identifier = "piano" + i;
-      int sound =
-        this.reactContext.getResources()
-          .getIdentifier(identifier, "raw", this.reactContext.getPackageName());
-      soundIds.put(identifier, soundPool.load(this.reactContext, sound, 1));
+    public PianoPlayerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        initializeSoundPool();
+        loadSounds();
     }
-  }
 
-  private Runnable playSound(
-    ReadableArray pianoNotes,
-    int streamId
-  ) {
-    return new Runnable() {
-      @Override
-      public void run() {
-        sendEvent(reactContext, "noteChange", streamId);
+    @Override
+    @NonNull
+    public String getName() {
+        return "PianoPlayerModule";
+    }
+
+    private void initializeSoundPool() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(15)
+                .setAudioAttributes(audioAttributes)
+                .build();
+    }
+
+    private void sendOnNoteChangeEvent(int beat) {
+        WritableMap params = Arguments.createMap();
+        params.putInt("num", beat);
+        getReactApplicationContext()
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("noteChange", params);
+    }
+
+    private void loadSounds() {
+        for (int i = 0; i <= 14; i++) {
+            String identifier = "piano" + i;
+            int sound = getReactApplicationContext().getResources()
+                    .getIdentifier(identifier, "raw", getReactApplicationContext().getPackageName());
+            soundIds.put(identifier, soundPool.load(getReactApplicationContext(), sound, 1));
+        }
+    }
+
+    private void playSound(ReadableArray pianoNotes, int streamId) {
+        sendOnNoteChangeEvent(streamId);
         for (int i = 0; i < pianoNotes.size(); i++) {
             int pianoNote = pianoNotes.getInt(i);
             int soundId = soundIds.get("piano" + pianoNote);
             int stream = soundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
             streamIds.put(streamId + i * 10000, stream);
         }
-      }
-    };
-  }
+    }
 
-  private Runnable stopSound(
-    ReadableArray pianoNotes,
-    int streamId
-  ) {
-    return new Runnable() {
-      @Override
-      public void run() {
+    private void stopSound(ReadableArray pianoNotes, int streamId) {
         for (int i = 0; i < pianoNotes.size(); i++) {
-          soundPool.stop(streamIds.get(streamId + i * 10000));
-          streamIds.remove(streamId + i * 10000);
+            soundPool.stop(streamIds.get(streamId + i * 10000));
+            streamIds.remove(streamId + i * 10000);
         }
-
-      }
-    };
-  }
-  
-  private Runnable endPlaying(Callback callback) {
-    return new Runnable() {
-      @Override
-      public void run() {
-        callback.invoke();
-      }
-    };
-  }
-
-  @ReactMethod
-  public void play(ReadableArray notes, Integer tempo, Callback onEnd) {
-    executor.setRemoveOnCancelPolicy(true);
-    streamIds = new HashMap<Integer, Integer>();
-    scheduledFutures = new ScheduledFuture[notes.size() * 2 + 2];
-    float noteDuration = 60.0f / tempo * 1000;
-    for (int i = 0; i < notes.size(); i++) {
-      ReadableMap note = notes.getMap(i);
-      ReadableArray pianoNotes = note.getArray("notes");
-      float time = i * noteDuration;
-      ScheduledFuture noteStart = executor.schedule(
-        playSound(
-          pianoNotes,
-          i
-        ),
-        (int)time,
-        TimeUnit.MILLISECONDS
-      );
-      float endTime = time + noteDuration + 100;
-      ScheduledFuture noteEnd = executor.schedule(
-        stopSound(
-          pianoNotes,
-          i
-        ),
-        (int)endTime,
-        TimeUnit.MILLISECONDS
-      );
-      scheduledFutures[i * 2] = noteStart;
-      scheduledFutures[i * 2 + 1] = noteEnd;
     }
 
-
-    float trackEndTime = noteDuration * notes.size();
-    ScheduledFuture playEnd = executor.schedule(
-      endPlaying(onEnd),
-      (int)trackEndTime,
-      TimeUnit.MILLISECONDS
-    );
-    scheduledFutures[notes.size() * 2 + 1] = playEnd;
-  }
-
-  @ReactMethod
-  public void stop() {
-    if (soundPool != null) {
-      for (int i = 0; i < scheduledFutures.length; i++) {
-        if (scheduledFutures[i] != null) {
-          scheduledFutures[i].cancel(false);
+    @ReactMethod
+    public void play(ReadableArray beats, double tempo, Promise promise) {
+        stop();
+        streamIds = new HashMap<>();
+        double noteDuration = 60.0f / tempo * 1000;
+        for (int i = 0; i < beats.size(); i++) {
+            ReadableArray notes = beats.getArray(i);
+            double delay = i * noteDuration;
+            int streamId = i;
+            handler.postDelayed(() -> playSound(notes, streamId), (long) delay);
+            handler.postDelayed(() -> stopSound(notes, streamId), (long) (delay + noteDuration));
         }
-      }
-      Iterator it = streamIds.entrySet().iterator();
-      while (it.hasNext()) {
-        Map.Entry<Integer, Integer> pair = (Map.Entry) it.next();
-        soundPool.stop(pair.getValue());
-        it.remove();
-      }
+        double endTime = noteDuration * beats.size();
+        handler.postDelayed(() -> promise.resolve(null), (long) endTime);
     }
-  }
+
+    @ReactMethod
+    public void stop() {
+        handler.removeCallbacksAndMessages(null);
+        if (soundPool != null) {
+            for (Integer streamId : streamIds.values()) {
+                soundPool.stop(streamId);
+            }
+        }
+        streamIds.clear();
+    }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,58 @@
-import React, { useState } from "react";
-import { Button, Pressable, ScrollView, View } from "react-native";
-import { SafeAreaProvider, useSafeAreaInsets, } from "react-native-safe-area-context";
+import React, {useState} from 'react';
+import {Button, Pressable, ScrollView, View} from 'react-native';
 import {
-  totalBars,
-  beatsPerBar,
-  notesInOctave,
-  totalOctaves,
-} from "./config";
-import styles from "./styles";
-import { INotes } from "./interfaces";
-import player from "./player";
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import {totalBars, beatsPerBar, notesInOctave, totalOctaves} from './config';
+import styles from './styles';
+import {INotes} from './interfaces';
+import usePlayer from './usePlayer';
 
 const App = () => {
   const insets = useSafeAreaInsets();
-  const [playingNote, isPlaying, playPitch, playTrack, stopTrack] = player();
+  const {
+    playingNote,
+    isPlaying,
+    playPitch,
+    play: playTrack,
+    stop: stopTrack,
+  } = usePlayer();
   const [notes, setNotes] = useState<INotes>(
-    Array.from({ length: totalBars }).map(() =>
-      Array.from({ length: beatsPerBar }).map(() => [])
-    )
+    Array.from({length: totalBars}).map(() =>
+      Array.from({length: beatsPerBar}).map(() => []),
+    ),
   );
   const handlePlay = () => {
     if (isPlaying) {
       stopTrack();
     } else {
-      playTrack(notes.reduce((all, bar) => [...all, ...bar], []))
+      playTrack(notes.reduce((all, bar) => [...all, ...bar], []));
     }
-  }
-  const handleToggleNote = (barIndex: number, beatIndex: number, pitchIndex: number) => {
-    setNotes(oldNotes => oldNotes.map((bar, barI) => {
-      if (barI !== barIndex) {
-        return bar;
-      }
-      return bar.map((beat, beatI) => {
-        if (beatI !== beatIndex) {
-          return beat;
+  };
+  const handleToggleNote = (
+    barIndex: number,
+    beatIndex: number,
+    pitchIndex: number,
+  ) => {
+    setNotes(oldNotes =>
+      oldNotes.map((bar, barI) => {
+        if (barI !== barIndex) {
+          return bar;
         }
-        if (beat.includes(pitchIndex)) {
-          return beat.filter(pitch => pitch !== pitchIndex);
-        }
-        playPitch(pitchIndex);
-        return [...beat, pitchIndex];
-      })
-    }))
-  }
+        return bar.map((beat, beatI) => {
+          if (beatI !== beatIndex) {
+            return beat;
+          }
+          if (beat.includes(pitchIndex)) {
+            return beat.filter(pitch => pitch !== pitchIndex);
+          }
+          playPitch(pitchIndex);
+          return [...beat, pitchIndex];
+        });
+      }),
+    );
+  };
   return (
     <View style={styles.container}>
       <ScrollView
@@ -52,35 +62,45 @@ const App = () => {
           paddingLeft: insets.left,
           paddingRight: insets.right,
         }}
-        showsHorizontalScrollIndicator={false}
-      >
-        {Array.from({ length: totalBars }).map((_, barIndex) => (
+        showsHorizontalScrollIndicator={false}>
+        {Array.from({length: totalBars}).map((_, barIndex) => (
           <View style={styles.bar} key={barIndex}>
-            {Array.from({ length: beatsPerBar }).map((_, beatIndex) => (
+            {Array.from({length: beatsPerBar}).map((_, beatIndex) => (
               <View style={styles.beat} key={beatIndex}>
-                {Array.from({ length: notesInOctave * totalOctaves + 1 }).map((_, pitchIndex) => (
-                  <Pressable
-                    onPress={() => handleToggleNote(barIndex, beatIndex, pitchIndex)}
-                    style={[
-                      styles.pitch,
-                      notes[barIndex][beatIndex].includes(pitchIndex) ? styles.selectedPitch : null,
-                      playingNote === (barIndex * beatsPerBar + beatIndex) ? styles.playingPitch : null,
-                      playingNote === (barIndex * beatsPerBar + beatIndex) && notes[barIndex][beatIndex].includes(pitchIndex) ? styles.playingSelectedPitch : null
-                    ]}
-                    key={pitchIndex}
-                  />
-                ))}
+                {Array.from({length: notesInOctave * totalOctaves + 1}).map(
+                  (_, pitchIndex) => (
+                    <Pressable
+                      onPress={() =>
+                        handleToggleNote(barIndex, beatIndex, pitchIndex)
+                      }
+                      style={[
+                        styles.pitch,
+                        notes[barIndex][beatIndex].includes(pitchIndex)
+                          ? styles.selectedPitch
+                          : null,
+                        playingNote === barIndex * beatsPerBar + beatIndex
+                          ? styles.playingPitch
+                          : null,
+                        playingNote === barIndex * beatsPerBar + beatIndex &&
+                        notes[barIndex][beatIndex].includes(pitchIndex)
+                          ? styles.playingSelectedPitch
+                          : null,
+                      ]}
+                      key={pitchIndex}
+                    />
+                  ),
+                )}
               </View>
             ))}
           </View>
         ))}
       </ScrollView>
       <View style={styles.tabBar}>
-          <Button title={isPlaying ? "Stop" : "Play"} onPress={handlePlay}/>
+        <Button title={isPlaying ? 'Stop' : 'Play'} onPress={handlePlay} />
       </View>
     </View>
   );
-}
+};
 
 export default () => (
   <SafeAreaProvider>
